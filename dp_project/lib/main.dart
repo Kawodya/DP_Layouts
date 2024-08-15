8fba446651249c25edcb97f918bc93966220238a
Lisitha Madurapperuma <lmadurapperuma10@gmail.com>
2024-08-15 10:27:06 +0530

    Changed to the new layout

--- dp_project/lib/main.dart
+++ dp_project/lib/main.dart
@@ -10,55 +10,107 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Layout 2",
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xff9E00FF),
-
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                height: 175,
-                width: double.infinity,
-                color: Color(0xff06FFA5),
-          
-              ),
-              Container(
-                height: 175,
-                width: double.infinity,
-                color: Color(0xff06FFA5),
-          
-          
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
+                    height: 175,
                     width: 175,
-                    height: 300,
-                    color: Color(0xffFFE500),
+                   
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xff06FFA5),
+                    ),
                   ),
                   Container(
-                     width: 175,
-                    height: 300,
-                    color: Color(0xffFFE500),
-          
+                    height: 175,
+                    width: 175,
+                   
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xff06FFA5),
+                    ),
                   ),
-                  
                 ],
+              ),
+              Container(
+                height: 175,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xff06FFA5),
+
+                ),
+              ),
+              SizedBox(
+                child: Container(
+                  height: 300,
+                
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 170,
+                            color:const Color(0xffFFE500),
+                            
+                          ),
+                          Container(
+                            height: 120,
+                            width: 170,
+                            color: const Color(0xffFFE500),
+                  
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        
+                        children: [
+                          Container(
+                            
+                            height: 90,
+                            width: 175,
+                            color: const Color(0xff7752FE),
+                  
+                          ),
+                           Container(
+                            height: 90,
+                            width: 175,
+                            color:  const Color(0xff0E00AC),
+                  
+                          ),
+                           Container(
+                            height: 90,
+                            width: 175,
+                            color:  const Color(0xff7752FE),
+                  
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
           
             ],
           ),
         ),
 
-
-        
       ),
     );
+    
   }
 }
